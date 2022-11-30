dee27fbce9f1f5246991aa8c2b5afc1dfc8c1d69
MarconiJunior <junior@brumafe.com>
2022-11-30 14:57:49 -0300

    Estilização da página Start

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import {View, ScrollView, Text, Image} from "react-native";
+import {View, ScrollView, Text, Image, StyleSheet} from "react-native";
 
 
 export default function Start() {
 
     return (
-        <View>
-            <ScrollView showVerticalScrollIndicator={false}>
-                <View>
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll} showVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
                     <Image 
                         source={require("../../assets/icons/logo3.png")}
+                        style={styles.logo}
                     />
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
                         melhor nível
                     </Text>
                 </View>
@@ -22,3 +23,25 @@ export default function Start() {
  
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, .98)"
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginTop: 60,
+        marginBottom: 60,
+    },
+});
