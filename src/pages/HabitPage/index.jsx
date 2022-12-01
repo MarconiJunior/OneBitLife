92d33e9506b45ea3a065f20818ae4be9dd2deda6
MarconiJunior <junior@brumafe.com>
2022-12-01 19:05:11 -0300

    DropdownList, selecionar a frequência

--- src/pages/HabitPage/index.jsx
+++ src/pages/HabitPage/index.jsx
@@ -11,10 +11,13 @@ import {
 import { useNavigation } from "@react-navigation/native";
 
 import SelectHabit from "../../components/HabitPage/SelectHabit";
+import SelectFrequency from "../../components/HabitPage/SelectFrequency";
 
 export default function HabitPage({ route }) {
 	const navigation = useNavigation();
   const [habitInput, setHabitInput] = useState();
+  const [frequencyInput, setFrequencyInput] = useState();
+
 	const { create, habit } = route.params;
 
   return (
@@ -38,6 +41,11 @@ export default function HabitPage({ route }) {
             </View>
             <Text style={styles.inputText}>Hábito</Text>
             <SelectHabit habit={habit} habitInput={setHabitInput} />
+            <Text style={styles.inputText}>Frequência</Text>
+            <SelectFrequency
+              habitFrequency={habit?.habitFrequency}
+              frequencyInput={setFrequencyInput}
+            />
           </View>
         </View>
       </ScrollView>
