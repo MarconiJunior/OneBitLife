acf3fba82636fb59b8d813652cc0a2a1b22fcf14
MarconiJunior <junior@brumafe.com>
2022-12-01 15:54:41 -0300

    Estrura das rotas e da HomePage

---
+++ src/pages/HabitPage/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+    Alert,
+} from "react-native"
+
+import { NavigationContainerRefContext, useNavigation } from "@react-navigation/native";
+
+export default function HabitPage() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <ScrollView>
+            <View>
+                <TouchableOpacity
+                    style={styles.backPageBtn}
+                    onPress={() => NavigationContainerRefContext.goBack()}
+                >
+                    <Image
+                        source={require("../../assets/icons/arrowBack.png")}
+                        style={styles.arrowBack}
+                    />
+                </TouchableOpacity>
+                <View style={styles.mainContent}></View>
+            </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, .98",
+    },
+
+    backPageBtn: {
+        width: 40,
+        height: 40,
+        margin: 25,
+    },
+
+    arrowBack: {
+        width: 40,
+        height: 25,
+    },
+
+    mainContent: {
+        width: 250,
+        alignSelf: "center",
+    },
+});
